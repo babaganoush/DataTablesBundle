dc5d9c8e9bf2607c03f0124fc67654f52ec8a358
Saud Faisal <saud@2levelsabove.com>
2014-10-21 08:30:03 -0500

    fixed namespace typo and some errors

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Babaganoush\DataTables\DependencyInjection;
+namespace Babaganoush\DataTablesBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
